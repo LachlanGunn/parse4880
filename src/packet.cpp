338ded93a3a5b31bad70c58d02c8610eb3c98ebb
Lachlan Gunn <lachlan@twopif.net>
2016-03-16 03:36:09 +0100

    Create UnknownPGPPackets in case of unsupported feature.

--- src/packet.cpp
+++ src/packet.cpp
@@ -17,22 +17,25 @@ PGPPacket::PGPPacket(std::string contents) : contents_(contents) {
 
 std::shared_ptr<PGPPacket> PGPPacket::ParsePacket(uint8_t tag,
                                                   std::string packet) {
-  switch (tag) {
-    case 2:
-      return std::shared_ptr<PGPPacket>(new SignaturePacket(packet));
-      break;
-    case 6:
-      return std::shared_ptr<PGPPacket>(new PublicKeyPacket(packet));
-      break;
-    case 13:
-      return std::shared_ptr<PGPPacket>(new UserIDPacket(packet));
-      break;
-    case 14:
-      return std::shared_ptr<PGPPacket>(new PublicSubkeyPacket(packet));
-      break;
-
-    default:
-      return std::shared_ptr<PGPPacket>(new UnknownPGPPacket(tag, packet));
+  try {
+    switch (tag) {
+      case 2:
+        return std::shared_ptr<PGPPacket>(new SignaturePacket(packet));
+        break;
+      case 6:
+        return std::shared_ptr<PGPPacket>(new PublicKeyPacket(packet));
+        break;
+      case 13:
+        return std::shared_ptr<PGPPacket>(new UserIDPacket(packet));
+        break;
+      case 14:
+        return std::shared_ptr<PGPPacket>(new PublicSubkeyPacket(packet));
+        break;
+      default:
+        return std::shared_ptr<PGPPacket>(new UnknownPGPPacket(tag, packet));
+    }
+  } catch (unsupported_feature_error e) {
+    return std::shared_ptr<PGPPacket>(new UnknownPGPPacket(tag, packet));
   }
 }
 
