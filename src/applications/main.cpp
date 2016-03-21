6a12073d613cf3c2d97d0005fb7693b80c60bc09
Lachlan Gunn <lachlan@twopif.net>
2016-03-22 00:35:30 +0100

    Change pgpparse to use callback-form of parser.

--- src/applications/main.cpp
+++ src/applications/main.cpp
@@ -11,14 +11,24 @@
 #include "exceptions.h"
 
 void print_packets(std::list<std::shared_ptr<parse4880::PGPPacket>> packets,
-                   int level) {
+                   int level);
+void print_packet(const parse4880::PGPPacket& packet, int level);
+
+
+void print_packet(const parse4880::PGPPacket& packet, int level) {
   std::string newline_replacement = "";
   for(int i = 0; i < level; i++) {
     newline_replacement += "    ";
   }
+  printf("%sPacket: %s\n", newline_replacement.c_str(), packet.str().c_str());
+  print_packets(packet.subpackets(), level+1);
+}
+
+void print_packets(std::list<std::shared_ptr<parse4880::PGPPacket>> packets,
+                   int level) {
+
   for (auto i = packets.begin(); i != packets.end(); i++) {
-    printf("%sPacket: %s\n", newline_replacement.c_str(), (*i)->str().c_str());
-    print_packets((*i)->subpackets(), level+1);
+    print_packet(**i, level);
   }
 }
 
@@ -34,16 +44,17 @@ int main(int argc, char** argv) {
   std::stringstream str_stream;
   str_stream << pgp_file.rdbuf();
 
-  std::list<std::shared_ptr<parse4880::PGPPacket>> packets;
   try {
     std::string file_contents = str_stream.str();
-    packets = parse4880::parse(
-        parse4880::ustring(file_contents.begin(), file_contents.end()));
+    parse4880::parse(
+        parse4880::ustring(file_contents.begin(), file_contents.end()),
+        [](std::shared_ptr<parse4880::PGPPacket> packet) -> bool {
+          print_packet(*packet, 0);
+          return true;
+        });
   }
   catch(const parse4880::parse4880_error& e) {
     fprintf(stderr, "Parse error:\n\t%s\n", e.what());
     return 1;
   }
-
-  print_packets(packets, 0);
 }
