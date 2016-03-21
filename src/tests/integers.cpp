5530a1dd983a04e90b628b1a1e7a7a7ce8c287bb
Lachlan Gunn <lachlan@twopif.net>
2016-03-21 07:35:17 +0100

    Vary length in integer roundtrip test.

--- src/tests/integers.cpp
+++ src/tests/integers.cpp
@@ -5,8 +5,8 @@
 
 TEST(ScalarNumbers, RoundTrip) {
   for (int length = 1; length < 4; length++) {
-    for (uint64_t i = 0; i < (1<<(length*8))+1; i++) {
-      ASSERT_EQ(i, parse4880::ReadInteger(parse4880::WriteInteger(i, 8)));
+    for (uint64_t i = 0; i < (1<<(length*8))-1; i++) {
+      ASSERT_EQ(i, parse4880::ReadInteger(parse4880::WriteInteger(i, length)));
     }
   }
 }
